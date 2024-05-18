6c54dc78a7a6571e8bc65d625de2bf537c540006
Sabbir Siddiqui <sabbir.siddiqui@optimizely.com>
2024-05-19 01:37:53 +0600

    fix: add teams

--- react-app/src/components/HomeUserTable/HomeUserTable.jsx
+++ react-app/src/components/HomeUserTable/HomeUserTable.jsx
@@ -1,7 +1,6 @@
 import { Button } from "react-bootstrap";
 import DataTable from "react-data-table-component";
 import { useNavigate } from "react-router-dom";
-import { daysDifference } from "../../services/utils";
 
 export function HomeUserTable({ userDataList }) {
   const navigate = useNavigate();
